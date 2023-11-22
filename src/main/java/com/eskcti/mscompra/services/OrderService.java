bf91bb30b24c25c7b8930149cf5e64de55e9dff4
eskokado <eskokado@gmail.com>
2023-11-22 13:27:52 -0300

    feat: impl producer into OrderService

--- src/main/java/com/eskcti/mscompra/services/OrderService.java
+++ src/main/java/com/eskcti/mscompra/services/OrderService.java
@@ -2,18 +2,23 @@ package com.eskcti.mscompra.services;
 
 import com.eskcti.mscompra.models.Order;
 import com.eskcti.mscompra.repositories.OrderRepository;
+import com.eskcti.mscompra.services.rabbitmq.Producer;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final Producer producer;
 
-    public OrderService(OrderRepository orderRepository) {
+    public OrderService(OrderRepository orderRepository, Producer producer) {
         this.orderRepository = orderRepository;
+        this.producer = producer;
     }
 
     public Order save(Order order) {
-        return orderRepository.save(order);
+        order = orderRepository.save(order);
+        producer.sendOrder(order);
+        return order;
     }
 }
