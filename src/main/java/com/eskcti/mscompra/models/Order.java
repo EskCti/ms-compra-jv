ae7aefecd79e614cb34dbb77883d4a820923de91
eskokado <eskokado@gmail.com>
2023-11-22 10:36:29 -0300

    fix: add annotation @Entity into Order class

--- src/main/java/com/eskcti/mscompra/models/Order.java
+++ src/main/java/com/eskcti/mscompra/models/Order.java
@@ -5,6 +5,8 @@ import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.*;
 
+import javax.persistence.*;
+import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.Date;
 
@@ -14,12 +16,24 @@ import java.util.Date;
 @Setter
 @Builder
 @JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
-public class Order {
+@Entity(name = "tb_orders")
+@ToString
+public class Order implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     private String name;
+
     private Long product;
-    private BigDecimal value;
+
+    private BigDecimal valueOrder;
+
     @JsonFormat(pattern = "yyyy-mm-dd")
     private Date purchaseDate;
+
     private String cpfCustomer;
+
     private String zipCode;
 }
