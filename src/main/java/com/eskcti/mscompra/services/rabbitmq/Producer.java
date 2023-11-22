4ccdd2d62602f10d64508a2d19cea1cd26ac90b5
eskokado <eskokado@gmail.com>
2023-11-22 14:33:34 -0300

    feat: impl consumer rabbitmq

--- src/main/java/com/eskcti/mscompra/services/rabbitmq/Producer.java
+++ src/main/java/com/eskcti/mscompra/services/rabbitmq/Producer.java
@@ -1,12 +1,12 @@
 package com.eskcti.mscompra.services.rabbitmq;
 
-import com.eskcti.mscompra.models.Order;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -20,7 +20,7 @@ public class Producer {
 
     @SneakyThrows
     @PostMapping
-    public void sendOrder(Order order) {
+    public void sendOrder(@Payload String order) {
         rabbitTemplate.convertAndSend(queue.getName(), mapper.writeValueAsString(order));
     }
 
