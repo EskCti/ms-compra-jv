39ea9728303a9450515b0b11724172e4b871f6cd
eskokado <eskokado@gmail.com>
2023-11-22 10:37:05 -0300

    feat: impl method save with order service into OrderController

--- src/main/java/com/eskcti/mscompra/resources/OrderController.java
+++ src/main/java/com/eskcti/mscompra/resources/OrderController.java
@@ -1,6 +1,7 @@
 package com.eskcti.mscompra.resources;
 
 import com.eskcti.mscompra.models.Order;
+import com.eskcti.mscompra.services.OrderService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,8 +11,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/orders")
 public class OrderController {
+    private final OrderService orderService;
+
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
     @PostMapping
     public ResponseEntity<Order> save(@RequestBody Order order) {
-        return ResponseEntity.ok(order);
+        return ResponseEntity.ok(orderService.save(order));
     }
 }
