c02306a601e75e2893d17a84794225181ba32300
eskokado <eskokado@gmail.com>
2023-11-26 14:13:12 -0300

    feat: add findOrFailById into OrderService

--- src/main/java/com/eskcti/mscompra/services/OrderService.java
+++ src/main/java/com/eskcti/mscompra/services/OrderService.java
@@ -2,6 +2,7 @@ package com.eskcti.mscompra.services;
 
 import com.eskcti.mscompra.models.Order;
 import com.eskcti.mscompra.repositories.OrderRepository;
+import com.eskcti.mscompra.services.exception.EntityNotFoundException;
 import com.eskcti.mscompra.services.rabbitmq.Producer;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,5 +20,10 @@ public class OrderService {
         order = orderRepository.save(order);
         producer.sendOrder(order);
         return order;
+    }
+
+    public Order findOrFailById(long id) {
+        return orderRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("O pedido de id: " + id + " nao existe na base de dados!"));
     }
 }
